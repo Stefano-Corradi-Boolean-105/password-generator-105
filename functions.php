4cdc9121fde0dbefefca0b1c8deecf1ee84a2d13
ugodeughi <stefano.corradi@gmail.com>
2023-10-30 10:53:51 +0100

    [add] controllo presenza di tutti  i tipi di caratteri

--- functions.php
+++ functions.php
@@ -1,9 +1,35 @@
 <?php
 
 function passwordGenerator($length){
-  // mischio la lista dei caratteri
-  $listChars = str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?&%$<>^+-*/()[]{}@#_=');
+  /*
+    1. ciclo in base a $length (con un while che e un contatore)
+    2. ad ogni ciclo prendo un valore random di ognuno delle stringhe e lo congateno
+    3. mischio la stringa ottenuta e la restituisco
+  
+  */
 
-  // prendo i prii caratteri della lista in base alla lunghezza scelta
-  return substr($listChars ,0 ,$length);
+  $psw = '';
+  $indexChars = 0;
+
+  $listChars = [
+      'abcdefghijklmnopqrstuvwxyz',
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      '0123456789',
+      '!?&%$<>^+-*/()[]{}@#_='
+  ];
+
+  // 1. 
+  while(strlen($psw) < $length){
+    // alterno le liste dei caratteri
+    $listChar = $listChars[$indexChars];
+    // 2 . 
+    $char = $listChar[rand(0, strlen($listChar) -1)];
+    $psw .= $char;
+    $indexChars++;
+    // se il contatore delle liste dei caratteri è troppo alto lo resetto
+    if($indexChars === count($listChars)) $indexChars = 0;
+  }
+
+  // 3.
+  return str_shuffle($psw );
 }
